5ae7b7f09a8af002738d52cf901edb5e1e293b6e
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-02 23:13:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,28 @@ src = [
 ]
 
 
+# def find_min_in_nested_arrays(src)
+# lowest_temps_array = []
+# row_index = 0
+# 
+#   while row_index < src.count
+#     element_index = 0
+#     while element_index < src[row_index].count
+#       weather_temp = 0
+#       if src[row_index][element_index] < weather_temp
+#       weather_temp = src[row_index][element_index]
+#       end
+# 
+#       lowest_temps_array << weather_temp
+#       element_index += 1
+#   end
+#   row_index += 1
+# end
+# lowest_temps_array
+# end
+# 
+# p find_min_in_nested_arrays(src)
+
 def find_min_in_nested_arrays(src)
 lowest_temps_array = []
 row_index = 0
@@ -19,10 +41,11 @@ row_index = 0
       weather_temp = 0
       if src[row_index][element_index] < weather_temp
       weather_temp = src[row_index][element_index]
+      element_index += 1
       end
 
       lowest_temps_array << weather_temp
-      element_index += 1
+      
   end
   row_index += 1
 end
