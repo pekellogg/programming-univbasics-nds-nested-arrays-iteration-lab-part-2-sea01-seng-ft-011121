275827e4f073adddca6125f04396772580d1b9c3
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-03 00:10:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -18,6 +18,7 @@ row_index = 0
   while row_index < src.length
     element_index = 0
     while element_index < src[row_index].length
+      weather_temp = 0
       if src[row_index][element_index] < weather_temp
         weather_temp = src[row_index][element_index]
       end
